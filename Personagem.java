c3503514fecd2b2f47d7f38e9cdaec2a052acad9
Marxsoel Elias <sr.marxelias@gmail.com>
2026-07-23 08:52:27 -0300

    Update Personagem.java

--- Personagem.java
+++ Personagem.java
@@ -1,97 +1,92 @@
+package eldoria;
+
 import java.util.Objects;
 
-public abstract class Personagem implements Comparable<Personagem>{
-	private String nome;
-	private String classe;
-	private int nivel;
-	private int vida;
-	private double poderBase;
-	
-//	CRIAÇÃO DO CONSTRUTOR DA CLASSE PERSONAGEM
-	public Personagem(String nome, String classe, int nivel, int vida, double poderBase) {
-		this.nome = nome;
-		this.classe = classe;
-		this.nivel = nivel; 
-		this.vida = vida;
-		this.poderBase = poderBase;
-	}
-	
-	public String getNome() {
-		return nome;
-	}
-	
-	public String getClasse() {
-		return classe;
-	}
-	
-	public int getNivel() {
-		return nivel;
-	}
-	
-	public int getVida() {
-		return vida;
-	}
-	
-	public double getPoderBase() {
-		return poderBase;
-	}
-	
-	public int compareTo(Personagem outro) {
-		if (this.nivel < outro.nivel) {
-			return -1;
-		} else if (this.nivel > outro.nivel) {
-			return 1;
-		} else {
-			return 0;
-		}
-	}
-	
-//	DEFINE O QUE EXIBIR COM INSTANCEOF PARA CADA CLASSE DE PERSONAGEM
-	public String instanciaPersonagem(Personagem p) {
-		String stringTexto = "";
-		
-		if (p instanceof Mago) {
-			stringTexto = "O personagem " + p.getNome() + " é um Mago de nivel " + 
-		p.getNivel();
-		} else if (p instanceof Guerreiro) {
-			stringTexto = "O personagem " + p.getNome() + " é um Guerreiro de nivel " +
-		p.getNivel() + "\n";
-		}
-		return stringTexto;
-	}
-	
-	@Override
-	public String toString() {
-		return "Personagem: " + nome + "\nClasse: " + classe + "\nNivel: " + nivel + 
-				"\nVida: " + vida + "\nPoder Base: " + poderBase;
-	}
-	
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) return true;
-		if (obj == null || getClass() != obj.getClass()) return false;
-		
-		Personagem func = (Personagem) obj;
-		return Objects.equals(nome, func.nome) &&
-			   Objects.equals(classe, func.classe);
-	}
-	
-	@Override
-	public int hashCode() {
-		return Objects.hash(nome, classe);
-	}
-	
-	public abstract String usarHabilidadeEspecial();
+/*
+ * Classe base para representar personagens do reino de Eldoria.
+ * Aplica conceitos de encapsulamento, herança e polimorfismo.
+ */
+public abstract class Personagem {
+
+    // Atributos privados (encapsulamento)
+    private String nome;
+    private String classe;
+    private int nivel;
+    private int pontosDeVida;
+    private double poderBase;
+
+    // Construtor que inicializa todos os atributos do personagem.
+    public Personagem(
+            String nome,
+            String classe,
+            int nivel,
+            int pontosDeVida,
+            double poderBase
+    ) {
+        this.nome = nome;
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
+    }
+
+    // Método que define a habilidade do personagem.
+    // Deve ser sobrescrito pelas subclasses para implementar comportamentos específicos de cada tipo de personagem.
+    public abstract void usarHabilidadeEspecial();
+
+    // Exibe as informações gerais do personagem
+    public void exibirStatus() {
+        System.out.println("\nNome.............: " + nome);
+        System.out.println("Classe...........: " + classe);
+        System.out.println("Nível............: " + nivel);
+        System.out.println("Pontos de vida...: " + pontosDeVida);
+        System.out.printf("Poder base.......: %.2f%n", poderBase);
+        //System.out.printf("Poder total......: %.2f%n", calcularPoderTotal());
+    }
+
+    // Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public String getClasse() {
+        return classe;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public int getPontosDeVida() {
+        return pontosDeVida;
+    }
+
+    public double getPoderBase() {
+        return poderBase;
+    }
+
+    // Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public void setPontosDeVida(int pontosDeVida) {
+        this.pontosDeVida = pontosDeVida;
+    }
+
+    public void setPoderBase(double poderBase) {
+        this.poderBase = poderBase;
+    }
+       
 }
-
-
-
-
-
-
-
-
-
 
 
 
