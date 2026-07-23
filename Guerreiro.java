d68428533f063585a32711beda89612b206e84d7
Marxsoel Elias <sr.marxelias@gmail.com>
2026-07-23 08:51:16 -0300

    Update Guerreiro.java

--- Guerreiro.java
+++ Guerreiro.java
@@ -1,10 +1,31 @@
+package eldoria;
+
 public class Guerreiro extends Personagem {
-	public Guerreiro(String nome, int nivel, int vida, double poderBase, double forca) {
-		super(nome, "Guerreiro", nivel, vida, poderBase);
-	}
-	
-	@Override
-	public String usarHabilidadeEspecial() {
-		return "\nAtaque Vórtice Esgamagador!";
-	}
+    
+    public Guerreiro(
+            String nome,
+            int nivel,
+            int pontosDeVida,
+            double poderBase,
+            double forca
+    ) {
+        super(
+                nome,
+                "Guerreiro",
+                nivel,
+                pontosDeVida,
+                poderBase
+        );
+
+    }
+
+    @Override
+        public void usarHabilidadeEspecial() {
+            System.out.printf(
+                    "Habilidade.......: %s desfere um golpe devastador!\n",
+                    getNome()
+            );
+        }
+
+       
 }
