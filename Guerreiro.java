aad974b24dd26849d6efe6719e4f51bd52ef298c
Marxsoel Elias <sr.marxelias@gmail.com>
2026-07-23 08:46:25 -0300

    Update Guerreiro.java

--- Guerreiro.java
+++ Guerreiro.java
@@ -1,25 +1,10 @@
-package eldoria;
-
-/**
- * Classe que representa um Guerreiro no reino de Eldoria.
- * Herda de Personagem e implementa habilidades específicas de combate.
- */
 public class Guerreiro extends Personagem {
-double forca;
-	
 	public Guerreiro(String nome, int nivel, int vida, double poderBase, double forca) {
 		super(nome, "Guerreiro", nivel, vida, poderBase);
-		this.forca = forca;
 	}
 	
 	@Override
-	public void usarHabilidadeEspecial() {
-		System.out.println("Guerreiro usou ataque esgamagador!\n");
-	}
-	
-	@Override
-	public String exibirStatus() {
-		return super.exibirStatus() + "\n" + "Força: " + Guerreiro.this.forca;
+	public String usarHabilidadeEspecial() {
+		return "\nAtaque Vórtice Esgamagador!";
 	}
 }
-
