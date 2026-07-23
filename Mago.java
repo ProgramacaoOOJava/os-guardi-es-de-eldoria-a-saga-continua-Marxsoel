91f6b611b6f961979a931837e09fc181e0829b90
Marxsoel Elias <sr.marxelias@gmail.com>
2026-07-23 08:36:43 -0300

    Update Mago.java

--- Mago.java
+++ Mago.java
@@ -5,19 +5,20 @@ package eldoria;
  * Herda de Personagem e implementa habilidades específicas de magia.
  */
 public class Mago extends Personagem {
-
     
-    // * Construtor para criar um Mago.
-
-
-
-    /**
-     * Sobrescrita do método usarHabilidade() específica para Magos.
-     * Demonstra polimorfismo e vinculação dinâmica.
-     */
-    @Override
-    public void usarHabilidade() {
-
-    }
+    int inteligencia;
+	
+	public Mago(String nome, int nivel, int vida, double poderBase, int inteligencia) {
+		super(nome, "Mago", nivel, vida, poderBase);
+		this.inteligencia = inteligencia;
+	}
+	
+	@Override
+	public void usarHabilidadeEspecial() {
+		System.out.println("Mago usou meteoros celestiais!\n");
+	}
+	
+	@Override
+	public String exibirStatus() {
+		return super.exibirStatus() + "\n" + "Inteligencia: " + Mago.this.inteligencia;
 }
-
